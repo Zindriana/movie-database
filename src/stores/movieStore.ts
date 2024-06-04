634c1868f3382ea144418800cbf14f7125d769b3
Zindriana <hulten.johan22@gmail.com>
2024-06-04 13:21:59 +0200

    updated movieStore

--- src/stores/movieStore.ts
+++ src/stores/movieStore.ts
@@ -1,15 +1,19 @@
 import { create } from 'zustand';
 import MovieType from "../models/MovieModel";
-//import filmerna från axiosanropet
 
 type MovieStore = {
     movieList: MovieType[];
+    setMovieList: (movie: MovieType[]) => void;
     addMovie: (movie: MovieType) => void;
-    deleteMovie: (movie: MovieType) => void;
+    deleteMovie: (title: string) => void;
 }
 
 const useMovieStore = create<MovieStore>((set) => ({
-    
+    movieList : [],
+    setMovieList : (movieList) => set({movieList}),
+    addMovie: (movie) => set((state) => ({movieList:[...state.movieList, movie]})),
+    deleteMovie: (title) => set((state) => ({movieList:state.movieList.filter((movie) => movie.title !== title)}))
+
 
 }));
 
